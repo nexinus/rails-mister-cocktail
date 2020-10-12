173bd77ac602eee019a3a3150680f32f20c22741
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-12 19:47:41 +0200

    Edit partial doses

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,5 @@ class Cocktail < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   # mount_uploader :photo, PhotoUploader
+  has_one_attached :photo
 end
